79a0b25faf56622c0422a4eb8352a64f4efe52a4
poojatripathi06 <58110449+poojatripathi06@users.noreply.github.com>
2020-01-31 02:04:13 +0530

    Update hangman2.py

--- hangman2.py
+++ hangman2.py
@@ -12,16 +12,19 @@ time.sleep(1)
 
 count = 0
 pick = list(word)
-show_letters = ['a', 'o', 'e', 'i', 'u']
-display = ''.join(letter if letter in show_letters else '*' for letter in pick)
+random_ele_pos = random.sample(range(len(pick)), k = 3)
+print(random_ele_pos)
+display = ''.join('*' if pos in random_ele_pos else letter for pos, letter in enumerate(pick))
 
 def hangman(count, display, word):
     limit = 3
 
     guess = input("This is word: " + display + " Enter your guess: ")
-    if guess in word:
-        index = word.find(guess)
-        word = word[:index] + "*" + word[index + 1:]
+    #remains need to handle reapeting characters
+    remains = ''.join(word[pos] if letter is '*' else '_' for pos, letter in enumerate(list(display)))
+    if guess in remains:
+        index = remains.find(guess)
+        #word = word[:index] + "*" + word[index + 1:]
         display = display[:index] + guess + display[index + 1:]
         print(display)
     else:
@@ -29,44 +32,45 @@ def hangman(count, display, word):
         if count == 1:
             print("Wrong input. " + str(limit - count) + " guess remaining ")
             print(
-                 " ______ \n"
+                 " __ \n"
                  " |      \n"
                  " |      \n"
                  " |      \n"
                  " |      \n"
                  " |      \n"
-                 " |      \n" 
-                 "_|__\n"
+                 " |      \n"
+                 "|_\n"
 
              )
 
         elif count == 2:
             print("Wrong input. " + str(limit - count) + " guess remaining ")
             print(
-                 " ______ \n"
+                 " __ \n"
                  " |    | \n"
                  " |    | \n"
                  " |    0 \n"
                  " |      \n"
                  " |      \n"
-                 " |      \n" 
-                 "_|__\n"
+                 " |      \n"
+                 "|_\n"
 
              )
 
         elif count == 3:
             print(" Wrong input. You are hanged! ")
-            print("   ____   \n"
+            print("   __   \n"
                   "  |   |   \n"
                   "  |   |   \n"
                   "  |   0   \n"
                   "  |  /|\  \n"
                   "  |  / \  \n"
                   "  |       \n"
-                  " _|__\n"
+                  " |_\n"
 
                   )
-    if word == '*' * length:
+    print(display)
+    if word == display:
          print("Congrats! You have guessed it successfully.....")
     elif count != limit:
          hangman(count, display, word)
